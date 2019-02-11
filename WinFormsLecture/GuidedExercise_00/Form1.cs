3bb4e63e39ea2e320544ba25e023f0153ff7d8ea
Steffan Mouton <smouton8993@gmail.com>
2019-02-11 17:22:24 -0600

    Added baseline GuidedExercise_00 (in class winforms)

---
+++ WinFormsLecture/GuidedExercise_00/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuidedExercise_00
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void WriteNewFile(object sender, EventArgs e)
+        {
+            ////This Works, but file address is hardcoded to THIS desktop.
+            //var pathToDesktop = "C:/Users/s188045/Desktop/GuidedExercise_00.txt";
+            //File.WriteAllText(pathToDesktop, "test text");
+            var pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var fileName = "GuidedExercise_00.txt";
+            var fullPath = pathDesktop + '/' + fileName;
+            File.WriteAllText( fullPath , "test text");
+        }
+
+     
+    }
+}
